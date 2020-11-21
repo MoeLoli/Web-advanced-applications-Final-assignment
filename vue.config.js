fa5b86995852b7a52d8c0c2826bdfeb7524d45af
Jin <moeprprpr@gmail.com>
2020-11-21 18:16:45 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Jin
+ * @Date: 2020-11-19 16:03:23
+ * @LastEditors: Jin
+ * @LastEditTime: 2020-11-21 18:16:35
+ * @FilePath: /final-assignment/vue.config.js
+ */
 module.exports = {
-    transpileDependencies: ["vuetify"]
+    transpileDependencies: ["vuetify"],
+    publicPath: "./",
+    runtimeCompiler: true
 };
